fd720e4701cffccf70e76856fe7a72f0940a6536
Nadine Kuo <h.n.kuo@student.tudelft.nl>
2024-05-26 10:33:36 +0200

    Post process results

--- code/utils.py
+++ code/utils.py
@@ -6,7 +6,6 @@ def json_to_list(data_path):
         data = json.load(f)
     return data
 
-
 # Extracts the model results between start and end tokens returned
 def extract_between_tags(text):
     start_tag = '<pad>'
@@ -24,4 +23,17 @@ def extract_between_tags(text):
     extracted_content = text[start_pos + len(start_tag):end_pos].strip()
     return extracted_content
 
-# TODO: Convert dict to Huggingface Dataset
+
+# Remove tokens for <pad> and </s> which correspond to 0 and 1
+# NOTE: check this per model tokenizer!
+def remove_pad_tokens(tokens_lst):
+    try:
+        # Find the index of the first occurrence of 0
+        start_index = tokens_lst.index(0)
+        # Find the index of the first occurrence of 1 after the 0
+        end_index = tokens_lst.index(1, start_index + 1)
+        # Slice the list from the first 0 to the first 1 (inclusive)
+        return tokens_lst[start_index + 1:end_index]
+    except ValueError:
+        # If 0 or 1 is not found, return the original list
+        return tokens_lst
