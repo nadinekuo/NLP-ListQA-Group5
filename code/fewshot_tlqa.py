4d895f65a129b5fe0e0bca39320aed4cd395dc09
Nadine Kuo <h.n.kuo@student.tudelft.nl>
2024-06-02 15:09:45 +0200

    Few-shot py script

---
+++ code/fewshot_tlqa.py
@@ -0,0 +1,91 @@
+from knn import KnnSearch
+from utils import json_to_list
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+import torch
+from datasets import Dataset
+from langchain_core.prompts.few_shot import FewShotPromptTemplate
+from langchain_core.prompts.prompt import PromptTemplate
+import accelerate   # TODO: uncomment when using GPU
+
+# NOTE: This script is based on TLQA_few_shot_ipynb
+
+
+# Extracts all questions from the (train) set used for getting neighbours
+def get_transfer_questions(transfer_data):
+    transfer_questions = []
+    for index, data in enumerate(transfer_data):
+        transfer_questions.append(data["question"])
+    return transfer_questions
+
+
+def simplify_dict_list(dict_list):
+    return [{'question': item['question'], 'answers': item['answers']} for item in dict_list]
+
+
+def fewshot_eval(K, model_name, test_data, train_data, train_emb):  
+    # TODO: Experiment with 3, 5, 7, 10
+    MAX_OUTPUT_LEN = 200
+    
+    model = T5ForConditionalGeneration.from_pretrained(model_name, device_map="auto")
+    tokenizer = T5Tokenizer.from_pretrained(model_name, torch_dtype=torch.float16)
+    
+    results_GT_dict = {'prompts': [], 'outputs': [], 'output_tokens': [], 
+                                'ground_truths': [], 'ground_truth_tokens': []}  
+
+    # Configure formatter that will format the few-shot examples into a string
+    example_prompt = PromptTemplate(
+        input_variables=["question", "answers"], template="Question: {question}\n{answers}"
+    )
+
+    # Convert test set to list and loop over all items (1071 in total)
+    for i, item in enumerate(test_data):
+        
+        # For each test question, retrieve k neighbours (try 3, 5, 7, 10)
+        test_question = test_data[i]['question']
+        print(f"Test question {i}: {test_question}")
+        neighs = knn.get_top_n_neighbours(sentence=test_question, data_emb=train_emb, transfer_data=train_data, k=K)
+        simple_neighs = simplify_dict_list(neighs)
+
+        # Create the few-shot prompt template and feed to model
+        prompt = FewShotPromptTemplate(
+            examples=simple_neighs,
+            example_prompt=example_prompt,
+            suffix="Question: {input}",
+            input_variables=["input"],
+        )
+        few_shot_prompt = prompt.format(input=f"{test_question} Please answer this question in the same format as the {K} examples above.")
+        results_GT_dict['prompts'].append(few_shot_prompt)
+        
+        # TODO: ---------------------- UNCOMMENT WHEN USING GPU -----------------------------
+        # input_ids = tokenizer(few_shot_prompt, return_tensors="pt").input_ids.to("cuda")
+        input_ids = tokenizer(few_shot_prompt, return_tensors="pt").input_ids
+        output_tokens = model.generate(input_ids, max_length=MAX_OUTPUT_LEN)
+        output = tokenizer.decode(output_tokens[0])
+
+        results_GT_dict['output_tokens'].append(output_tokens[0])
+        results_GT_dict['outputs'].append(output)
+        results_GT_dict['ground_truths'].append(test_data[i]['final_answers'])
+        gt_tokens = tokenizer(str(test_data[i]['final_answers']), return_tensors="pt").input_ids[0]
+        results_GT_dict['ground_truth_tokens'].append(gt_tokens)
+
+    results_ds = Dataset.from_dict(results_GT_dict)
+    results_ds.save_to_disk(f"{K}_shot_{model_name}.hf")
+
+
+
+if __name__ == '__main__':
+
+    Ks = [3, 5, 7, 10]
+    
+    knn = KnnSearch()
+    test_set = json_to_list("../data/test_TLQA.json")
+    train_set = json_to_list("../data/train_TLQA.json")
+    train_questions = get_transfer_questions(train_set)   # Keep questions only to embed (to use in similarity metric)
+    train_questions_emb = knn.get_embeddings_for_data(train_questions)
+
+    for k in Ks:
+        print(f"\n\nStarting {k}-shot evaluation on FlanT5-large...\n\n")
+        fewshot_eval(K=k, model_name="google/flan-t5-large", test_data=test_set, train_data=train_set, train_emb=train_questions_emb)
+        print(f"\n\nStarting {k}-shot evaluation on FlanT5-XL...\n\n")
+        fewshot_eval(K=k, model_name="google/flan-t5-xl", test_data=test_set, train_data=train_set, train_emb=train_questions_emb)
+
---
+++ code/knn.py
@@ -0,0 +1,39 @@
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+class KnnSearch:
+    def __init__(self, data=None, num_trees=None, emb_dim=None):
+        self.num_trees = num_trees
+        self.emb_dim = emb_dim
+
+    def get_embeddings_for_data(self, data_ls):
+        # NOTE: any embedding model from sentence-transformers can be used
+        model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+        embeddings = model.encode(data_ls)
+        return embeddings
+
+    def get_top_n_neighbours(self, sentence, data_emb, transfer_data, k):
+        """
+        Retrieves the top k most similar questions for "sentence" based on cosine similarity from given embeddings "data_emb".
+
+        Parameters:
+        sentence (str): The input sentence to find similar questions for.
+        data_emb (np.ndarray): The embeddings for the transfer questions.
+        transfer_data (list): The list of transfer questions corresponding to data_emb.
+        k (int): The number of top similar questions to retrieve.
+
+        Returns:
+        list: A list of the top k similar questions from transfer_data and all similar questions from str_qa.
+        """
+        sent_emb = self.get_embeddings_for_data(sentence)
+        top_questions = []
+
+        text_sims = cosine_similarity(data_emb,[sent_emb]).tolist()
+        results_sims = zip(range(len(text_sims)), text_sims)
+        sorted_similarities = sorted(results_sims, key=lambda x: x[1], reverse=True)  # Obtain the highest similarities
+
+        # NOTE: we only match based on questions, but include the full question-answer pair in resulting neighs
+        for idx, item in sorted_similarities[:k]:
+                    top_questions.append(transfer_data[idx])
+
+        return top_questions
