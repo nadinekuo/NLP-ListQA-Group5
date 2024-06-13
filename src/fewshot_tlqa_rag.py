7f77cbf9255447664027d51f8793fcbf51a69947
Nadine Kuo <h.n.kuo@student.tudelft.nl>
2024-06-13 13:22:25 +0200

    Fix spacing in few shot prompt

--- src/fewshot_tlqa_rag.py
+++ src/fewshot_tlqa_rag.py
@@ -156,9 +156,9 @@ def fewshot_eval_with_context(K, model_name, test_data, train_data, train_emb, i
             input_variables=["input"],
         )
         few_shot_prompt = prompt.format(input=f"{test_question}\nPlease answer this question in the same format as the {K} examples above.\n\n\
-                                            Use the following context to answer the question at the end. Do not use any other information.\
-                                            If you can't find the relevant information in the context, just say you don't have enough information to answer the question.\
-                                            Don't try to make up an answer.\n\n{top_infobox}")
+        Use the following context to answer the question at the end. Do not use any other information. \
+        If you can't find the relevant information in the context, just say you don't have enough information to answer the question. \
+        Don't try to make up an answer.\n\n{top_infobox}")
 
         # Print the prompt to see how it looks
         # print(f"Few-shot Prompt for Test Question {i}:\n{few_shot_prompt}\n")
